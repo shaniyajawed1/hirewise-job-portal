c8b1eb1c6408dd12fe06a3766cd759a8e1ecff4d
shaniyajawed1 <shaniyajawed1gmail.com>
2025-07-28 15:48:56 +0530

    Added full Spring Boot project files

---
+++ src/main/java/com/hirewise/jobportal/HirewiseJobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.hirewise.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirewiseJobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirewiseJobPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hirewise/jobportal/HirewiseJobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirewise.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirewiseJobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
